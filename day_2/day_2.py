f58d7d38df684b43920180320a5119b454f4fd1a
hadiChowdhury <chowdhuryhadiaiub20@gmail.com>
2025-08-02 13:15:30 +0600

    day two

---
+++ day_2/day_2.py
@@ -0,0 +1,32 @@
+first_name = 'Hadi'
+last_name = 'Chowdhury'
+country = 'Bangladesh'
+city = 'Dhaka'
+age = 24
+is_married = True
+skills = ['Python', 'ZeuZ', 'Postman', 'HTML', 'CSS', 'ReactJs']
+person_info = {
+   'firstname':'Hadi',
+   'lastname':'Chowdhury',
+   'country':'Bangladesh',
+   'city':'Dhaka'
+   }
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+first_name = input('What is your name: ')
+age = input('How old are you? ')
+
+print(first_name)
+print(age)
---
+++ day_2/variables.py
@@ -0,0 +1,46 @@
+# Day 2: 30 Days of python programming
+
+first_name = 'Hadi '
+last_name = 'Chowdhury'
+full_name = first_name  + last_name
+country = 'Bangladesh'
+city = 'Dhaka'
+age = 24
+year = 2025
+is_married = True
+is_true = True
+is_light_on = True
+
+passport, nid = 'A0213124', '0981231511'
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+print(len(last_name))
+
+if len(first_name) < len(last_name):
+    print(is_true)
+else:
+    print("false")
+    
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+
+exp = num_one ** num_two
+print(remainder)
