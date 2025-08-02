a4f1d670ff69dcf8bc2918a718b06bfea15d8783
hadiChowdhury <chowdhuryhadiaiub20@gmail.com>
2025-08-03 00:48:44 +0600

    day_4

---
+++ day_3/day_3.py
@@ -0,0 +1,26 @@
+age = 24
+height = 173.5
+complex_number = (1 + 3j)
+
+'''
+print("++ Calculate the area of triangle")
+
+base = input('Enter base ')
+height = input('Enter height ')
+
+cal = 0.5 * int(base) * int(height)
+
+print("area of the triangle = ", int(cal))
+
+
+print("++ Calculate the perimeter of the triangle")
+
+side_a = int(input('Enter side a: '))
+side_b = int(input('Enter side b: '))
+side_c = int(input('Enter side c: '))
+
+perimeter = side_a + side_b + side_c
+
+print('The perimeter of the triangle is ', perimeter)
+
+'''
---
+++ day_4/day_4.py
@@ -0,0 +1,10 @@
+'''
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+language = 'Python'
+formated_string = 'I am %s %s. I teach %s' %(first_name, last_name, language)
+print(formated_string)
+'''
+
+company = 'Coding For All'
+print(company.swapcase())
