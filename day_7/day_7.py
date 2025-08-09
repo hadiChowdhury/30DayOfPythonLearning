730b377185bb3b0f864eec6fc59225e9773765ab
hadiChowdhury <chowdhuryhadiaiub20@gmail.com>
2025-08-09 22:56:56 +0600

    Day 8

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -3,13 +3,16 @@ it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Am
 A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
 age = [22, 19, 24, 25, 26, 24, 25, 24]
-print(len(it_companies))
 
+#1
+print(len(it_companies))
+#2
 it_companies.add('Twitter')
 print(it_companies)
+#3
 it_companies.update(['Meta', 'ZeuZ', 'BB'])
 print(it_companies)
-
+#4
 it_companies.remove('Meta')
 print(it_companies)
 
---
+++ day_8/day_8.py
@@ -0,0 +1,28 @@
+#1
+dog = {}
+print(type(dog))
+
+#2
+dog['name'] = 'ZOY'
+dog['color'] = 'Black'
+dog['breed'] = 'German'
+dog['legs'] = 4
+dog['age'] = 12
+
+print(dog)
+
+student = {
+    'first_name' : 'Shihab',
+    'last_name' : 'Chowdhury',
+    'gender' : 'male',
+    'age' : 24,
+    'marital_status': 'single',
+    'skills':['Linux', 'React', 'Node', 'Python'],
+    'country' : 'Bangladesh',
+    'city' : 'Dhaka',
+    'address': { 
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+}
+
